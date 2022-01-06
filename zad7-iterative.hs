d98f86b16d854aad8606992db54c86b4eda071f2
Marta Rybarczyk <martarybarczyk98@gmail.com>
2022-01-06 14:27:10 +0100

    oops did iterative and recursive in wrong files

--- zad7-iterative.hs
+++ zad7-iterative.hs
@@ -4,3 +4,20 @@
 -- silnia n = silnia_help n 1
 --    where silnia_help 0 acc = acc
 --          silnia_help n acc = silnia_help (n - 1) (n * acc)
+
+fib :: Integer -> Integer
+fib n = do
+ let n1 = 0
+ let n2 = 1
+ fibonacci_recursive n n1 n2
+
+fibonacci_recursive :: Integer -> Integer -> Integer -> Integer
+fibonacci_recursive n n1 n2 = do
+ if n == 0
+  then do
+   n
+ else if n <= 1 
+  then do
+   n2
+  else 
+   fibonacci_recursive (n-1) (n2) (n1+n2)
--- zad7-recursive.hs
+++ zad7-recursive.hs
@@ -3,17 +3,10 @@
 
 fib :: Integer -> Integer
 fib n = do
- let n1 = 0
- let n2 = 1
- fibonacci_recursive n n1 n2
-
-fibonacci_recursive :: Integer -> Integer -> Integer -> Integer
-fibonacci_recursive n n1 n2 = do
- if n == 0
+ if n <= 1 
   then do
    n
- else if n <= 1 
-  then do
-   n2
   else 
-   fibonacci_recursive (n-1) (n2) (n1+n2)
+   fib (n-1) + fib (n-2)
+
+ 
